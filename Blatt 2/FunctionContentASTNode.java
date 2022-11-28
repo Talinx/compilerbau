8bc7dae8fbf40bb0ee291d7b03c0a1ef18159051
Niklas Theis <39201560+TheisNiklas@users.noreply.github.com>
2022-11-28 14:47:05 +0100

    Fix ast

--- Blatt 2/FunctionContentASTNode.java
+++ Blatt 2/FunctionContentASTNode.java
@@ -1,7 +1,11 @@
+import java.util.List;
+
 public class FunctionContentASTNode extends ASTNode {
+	List<ASTNode> statements;
 	ASTNode returnExpr;
 	
-	public FunctionContentASTNode(ASTNode returnExpr) {
+	public FunctionContentASTNode(List<ASTNode> statements, ASTNode returnExpr) {
+		this.statements = statements;
 		this.returnExpr = returnExpr;
 	}
 }
