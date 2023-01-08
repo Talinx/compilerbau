8625652cdb66b0221f43ddc6005ac5586df90794
Christoph Raitzig <chris-r@posteo.net>
2023-01-08 16:06:46 +0100

    Compile and run with Makefile

---
+++ Blatt 4/generateAntlrFiles.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd src/main/java/compilerbau/
+mkdir -p tempantlr
+cp MiniPython.g4 tempantlr/
+cd tempantlr
+antlr4 MiniPython.g4 -visitor
+cd ..
+cp tempantlr/*.interp .
+cp tempantlr/*.tokens .
+for f in tempantlr/*.java
+do
+	fb=$(basename $f)
+	echo -e "package compilerbau;\n\n" > $fb
+	echo -e "package compilerbau;\n\n"
+	cat $f >> $fb
+done
+rm -r tempantlr
+cd ../../../..
---
+++ Blatt 4/interpret.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -classpath /usr/share/java/antlr4-runtime.jar:./target/classes compilerbau.MiniPythonInterpreter
